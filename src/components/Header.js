bac76da359eb9b21a4d9176942e90b32801013f0
husseinhareb <shtam.hareb12@gmail.com>
2023-10-30 10:08:40 +0200

    header setup

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Header()
+{
+    return (
+        <header className="header">
+            <img className="header--image" src="/images/troll-face.png "/>
+            <h2 className="header--title">Meme Generator</h2>
+        </header>
+    )
+}
