b45d7b9bc33ea2e7a2234aaf95709f783072cf3b
husseinhareb <shtam.hareb12@gmail.com>
2023-10-30 10:55:38 +0200

    meme component

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -2,13 +2,19 @@ import React from "react"
 
 export default function Meme()
 {
+
+    function getMemeImage()
+    {
+        console.log("ge")
+    }
+
     return (
         <main>
-            <form className="form">
+            <div className="form">
             <input className="form--input" type="text" placeholder="Top Text" />
             <input className="form--input" type="text" placeholder="Buttom Text"/>
-            <button className="form--button">Get a new meme image</button>
-            </form>
+            <button className="form--button" onClick={getMemeImage}>Get a new meme image</button>
+            </div>
         </main>
     )
 }
